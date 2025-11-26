831cdc51f944a349dd72f9a9be2c82fea16e0899
Codewithkaranja <codewithkaranja@gmail.com>
2025-11-26 08:12:49 +0300

    Updated Applications section with 3D Swiper, improved card layout, and styling fixes

--- index.js
+++ index.js
@@ -225,3 +225,29 @@ document.addEventListener("DOMContentLoaded", function () {
     heroTouch.addEventListener("mouseleave", startCycle);
   }
 });
+
+ //swiper slider
+      var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 30,
+      stretch: 0,
+      depth: 200,
+      modifier: 1,
+      slideShadows: true,
+    },
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  
+  
